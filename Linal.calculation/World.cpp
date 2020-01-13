44731df90928d1d7601dfea1419748bb1f7fa47f
BestTommygun <tac.peters@student.avans.nl>
2020-01-13 17:19:38 +0100

    updated vertexes

--- Linal.calculation/World.cpp
+++ Linal.calculation/World.cpp
@@ -13,102 +13,137 @@ PlayerObject World::makePlayer(Vector3d position)
 	vertexes.push_back(Vector3d(-2.094380, 0.500000, - 0.000000));
 	vertexes.push_back(Vector3d(-2.094380, 0.000000, 0.500000));
 
-	vertexes.push_back(Vector3d(-2.094380 0.000000 - 0.500000));
-	vertexes.push_back(Vector3d(-2.978710 0.000000 - 0.000000));
-	vertexes.push_back(Vector3d(-0.468188 - 0.628495 - 0.628495));
-	vertexes.push_back(Vector3d(-0.468188 - 0.628495 0.628495));
+	vertexes.push_back(Vector3d(-2.094380, 0.000000, - 0.500000));
+	vertexes.push_back(Vector3d(-2.978710, 0.000000, - 0.000000));
+	vertexes.push_back(Vector3d(-0.468188, - 0.628495, - 0.628495));
+	vertexes.push_back(Vector3d(-0.468188, - 0.628495, 0.628495));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(-0.468187, 0.628495, - 0.628495));
+	vertexes.push_back(Vector3d(-0.468188, 0.628495, 0.628496));
+	vertexes.push_back(Vector3d(-0.481333, 0.628495, 0.000000));
+	vertexes.push_back(Vector3d(-0.481333, - 0.734441, - 0.000000));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(-0.481333, 0.000000, - 0.844413));
+	vertexes.push_back(Vector3d(-0.481333, 0.000000, 0.844413));
+	vertexes.push_back(Vector3d(0.024449, 0.633387, 0.000000));
+	vertexes.push_back(Vector3d(0.025334, - 0.633387, - 0.633387));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(0.025334, - 0.633387, 0.633387));
+	vertexes.push_back(Vector3d(0.025334, 0.633387, - 0.633386));
+	vertexes.push_back(Vector3d(0.025333, 0.633387, 0.633387));
+	vertexes.push_back(Vector3d(0.024449, - 0.739333, - 0.000000));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(0.024449, 0.000000, - 1.696315));
+	vertexes.push_back(Vector3d(0.024449, 0.000000, 1.696315));
+	vertexes.push_back(Vector3d(1.319742, 0.718534, 0.000000));
+	vertexes.push_back(Vector3d(1.320421, - 0.294940, - 0.506737));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(1.320421, - 0.294940, 0.506737));
+	vertexes.push_back(Vector3d(1.320422, 0.718534, - 0.506737));
+	vertexes.push_back(Vector3d(1.320421, 0.718534, 0.506737));
+	vertexes.push_back(Vector3d(1.319742, - 0.400886, 0.000000));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(1.319743, 0.211797, - 1.359128));
+	vertexes.push_back(Vector3d(1.319742, 0.211797, 1.359128));
+	vertexes.push_back(Vector3d(2.312777, 0.599662, 0.000000));
+	vertexes.push_back(Vector3d(2.313055, 0.148638, - 0.225512));
 
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(2.313055, 0.148638, 0.225512));
+	vertexes.push_back(Vector3d(2.313055, 0.599662, - 0.225512));
+	vertexes.push_back(Vector3d(2.313054, 0.599662, 0.225512));
+	vertexes.push_back(Vector3d(2.312777, 0.042692, 0.000000));
 
-	vertexes.push_back(Vector3d());
+	vertexes.push_back(Vector3d(2.312777, 0.374150, - 0.606505));
+	vertexes.push_back(Vector3d(2.312777, 0.374150, 0.606506));
 
 	std::vector<size_t> triangles = std::vector<size_t>();
 	//front side
-	triangles.push_back(0);
-	triangles.push_back(1);
+	triangles.push_back(16);
 	triangles.push_back(2);
+	triangles.push_back(6);
 
-	triangles.push_back(1);
-	triangles.push_back(2);
-	triangles.push_back(3);
+	triangles.push_back(15);
+	triangles.push_back(4);
+	triangles.push_back(14);
 
 	//left side
-	triangles.push_back(0);
+	triangles.push_back(18);
 	triangles.push_back(4);
-	triangles.push_back(2);
+	triangles.push_back(8);
 
-	triangles.push_back(2);
-	triangles.push_back(6);
-	triangles.push_back(4);
+	triangles.push_back(7);
+	triangles.push_back(9);
+	triangles.push_back(10);
 
 	//top side
-	triangles.push_back(2);
-	triangles.push_back(3);
-	triangles.push_back(6);
+	triangles.push_back(17);
+	triangles.push_back(9);
+	triangles.push_back(10);
 
-	triangles.push_back(6);
+	triangles.push_back(17);
 	triangles.push_back(3);
-	triangles.push_back(7);
+	triangles.push_back(9);
 
 	//bottom side
-	triangles.push_back(0);
-	triangles.push_back(1);
-	triangles.push_back(4);
+	triangles.push_back(8);
+	triangles.push_back(7);
+	triangles.push_back(10);
 
-	triangles.push_back(1);
-	triangles.push_back(4);
 	triangles.push_back(5);
+	triangles.push_back(15);
+	triangles.push_back(13);
 
 	//right side
-	triangles.push_back(1);
+	triangles.push_back(16);
 	triangles.push_back(3);
-	triangles.push_back(5);
+	triangles.push_back(11);
 
-	triangles.push_back(1);
-	triangles.push_back(5);
-	triangles.push_back(7);
+	triangles.push_back(8);
+	triangles.push_back(6);
+	triangles.push_back(2);
 
 	//back side
-	triangles.push_back(4);
+	triangles.push_back(17);
 	triangles.push_back(5);
-	triangles.push_back(6);
+	triangles.push_back(13);
 
-	triangles.push_back(5);
 	triangles.push_back(6);
+	triangles.push_back(9);
+	triangles.push_back(3);
+
+	triangles.push_back(18);
+	triangles.push_back(2);
+	triangles.push_back(12);
+
+	triangles.push_back(21);
+	triangles.push_back(18);
+	triangles.push_back(12);
+
+	triangles.push_back(22);
+	triangles.push_back(17);
+	triangles.push_back(13);
+
+	triangles.push_back(24);
+	triangles.push_back(11);
+	triangles.push_back(20);
+
+	triangles.push_back(13);
+	triangles.push_back(19);
+	triangles.push_back(22);
+
+	triangles.push_back(13);
+	triangles.push_back(19);
+	triangles.push_back(22);
+
+	triangles.push_back(20);
+	triangles.push_back(17);
+	triangles.push_back(25);
+
+	triangles.push_back(6);
+	triangles.push_back(3);
 	triangles.push_back(7);
+
+
 
 	Mesh cubeMesh = Mesh(vertexes, triangles); //TODO: one of these triangles is wrong, see the render 
 	BoundingBox cubeHitBox = BoundingBox(Vector3d(-1.5, -1.5, -1.5), Vector3d(1.5, 1.5, 1.5));
