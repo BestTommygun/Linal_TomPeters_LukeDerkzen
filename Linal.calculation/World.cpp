784cda7a61877bd6da6a99d53dd390c0ea8a76b3
BestTommygun <tac.peters@student.avans.nl>
2020-01-13 17:42:45 +0100

    added supercool spaceship

--- Linal.calculation/World.cpp
+++ Linal.calculation/World.cpp
@@ -3,6 +3,7 @@
 PlayerObject World::makePlayer(Vector3d position) 
 {
 	std::vector<Vector3d> vertexes = std::vector<Vector3d>();
+	vertexes.push_back(Vector3d(0, 0, 0));
 	vertexes.push_back(Vector3d(3.451090, 0.520436, 0.004318));
 	vertexes.push_back(Vector3d(-1.759147, - 0.500000, 0.500000));
 	vertexes.push_back(Vector3d(-1.759146, - 0.500000, - 0.500000));
@@ -139,9 +140,257 @@ PlayerObject World::makePlayer(Vector3d position)
 	triangles.push_back(17);
 	triangles.push_back(25);
 
+	triangles.push_back(23);
+	triangles.push_back(18);
+	triangles.push_back(26);
+
+	triangles.push_back(19);
+	triangles.push_back(14);
+	triangles.push_back(23);
+
+	triangles.push_back(24);
+	triangles.push_back(12);
+	triangles.push_back(16);
+
+	triangles.push_back(32);
+	triangles.push_back(21);
+	triangles.push_back(24);
+
+	triangles.push_back(19);
+	triangles.push_back(31);
+	triangles.push_back(27);
+
+	triangles.push_back(34);
+	triangles.push_back(23);
+	triangles.push_back(26);
+
+	triangles.push_back(33);
+	triangles.push_back(20);
+	triangles.push_back(25);
+
+	triangles.push_back(30);
+	triangles.push_back(19);
+	triangles.push_back(27);
+
+	triangles.push_back(32);
+	triangles.push_back(20);
+	triangles.push_back(28);
+
+	triangles.push_back(33);
+	triangles.push_back(22);
+	triangles.push_back(30);
+
+	triangles.push_back(34);
+	triangles.push_back(21);
+	triangles.push_back(29);
+
+	triangles.push_back(42);
+	triangles.push_back(29);
+	triangles.push_back(37);
+
+	triangles.push_back(41);
+	triangles.push_back(30);
+	triangles.push_back(38);
+
+	triangles.push_back(40);
+	triangles.push_back(28);
+	triangles.push_back(36);
+
+	triangles.push_back(38);
+	triangles.push_back(27);
+	triangles.push_back(35);
+
+	triangles.push_back(41);
+	triangles.push_back(28);
+	triangles.push_back(33);
+
+	triangles.push_back(42);
+	triangles.push_back(31);
+	triangles.push_back(34);
+
+	triangles.push_back(27);
+	triangles.push_back(39);
+	triangles.push_back(35);
+
+	triangles.push_back(40);
+	triangles.push_back(29);
+	triangles.push_back(32);
+
+	triangles.push_back(1);
+	triangles.push_back(37);
+	triangles.push_back(40);
+
+	triangles.push_back(1);
+	triangles.push_back(35);
+	triangles.push_back(39);
+
+	triangles.push_back(1);
+	triangles.push_back(39);
+	triangles.push_back(42);
+
+	triangles.push_back(1);
+	triangles.push_back(36);
+	triangles.push_back(41);
+
+	triangles.push_back(1);
+	triangles.push_back(38);
+	triangles.push_back(35);
+
+	triangles.push_back(1);
+	triangles.push_back(40);
+	triangles.push_back(36);
+
+	triangles.push_back(1);
+	triangles.push_back(41);
+	triangles.push_back(38);
+
+	triangles.push_back(1);
+	triangles.push_back(42);
+	triangles.push_back(37);
+
+	triangles.push_back(16);
+	triangles.push_back(12);
+	triangles.push_back(2);
+
+	triangles.push_back(15);
+	triangles.push_back(7);
+	triangles.push_back(4);
+
+	triangles.push_back(18);
+	triangles.push_back(14);
+	triangles.push_back(4);
+
+	triangles.push_back(7);
+	triangles.push_back(5);
+	triangles.push_back(9);
+
+	triangles.push_back(17);
+	triangles.push_back(11);
+	triangles.push_back(3);
+
+	triangles.push_back(8);
+	triangles.push_back(4);
+	triangles.push_back(7);
+
+	triangles.push_back(5);
+	triangles.push_back(7);
+	triangles.push_back(15);
+
+	triangles.push_back(16);
 	triangles.push_back(6);
 	triangles.push_back(3);
-	triangles.push_back(7);
+
+	triangles.push_back(8);
+	triangles.push_back(10);
+	triangles.push_back(6);
+
+	triangles.push_back(17);
+	triangles.push_back(9);
+	triangles.push_back(5);
+
+	triangles.push_back(6);
+	triangles.push_back(10);
+	triangles.push_back(9);
+
+	triangles.push_back(18);
+	triangles.push_back(8);
+	triangles.push_back(2);
+
+	triangles.push_back(21);
+	triangles.push_back(26);
+	triangles.push_back(18);
+
+	triangles.push_back(22);
+	triangles.push_back(25);
+	triangles.push_back(17);
+
+	triangles.push_back(24);
+	triangles.push_back(16);
+	triangles.push_back(11);
+
+	triangles.push_back(13);
+	triangles.push_back(15);
+	triangles.push_back(19);
+
+	triangles.push_back(20);
+	triangles.push_back(11);
+	triangles.push_back(17);
+
+	triangles.push_back(23);
+	triangles.push_back(14);
+	triangles.push_back(18);
+
+	triangles.push_back(19);
+	triangles.push_back(15);
+	triangles.push_back(14);
+
+	triangles.push_back(24);
+	triangles.push_back(21);
+	triangles.push_back(12);
+
+	triangles.push_back(32);
+	triangles.push_back(29);
+	triangles.push_back(21);
+
+	triangles.push_back(19);
+	triangles.push_back(23);
+	triangles.push_back(31);
+
+	triangles.push_back(34);
+	triangles.push_back(31);
+	triangles.push_back(23);
+
+	triangles.push_back(33);
+	triangles.push_back(28);
+	triangles.push_back(20);
+
+	triangles.push_back(30);
+	triangles.push_back(22);
+	triangles.push_back(19);
+
+	triangles.push_back(32);
+	triangles.push_back(24);
+	triangles.push_back(20);
+
+	triangles.push_back(33);
+	triangles.push_back(25);
+	triangles.push_back(22);
+
+	triangles.push_back(34);
+	triangles.push_back(26);
+	triangles.push_back(21);
+
+	triangles.push_back(42);
+	triangles.push_back(34);
+	triangles.push_back(29);
+
+	triangles.push_back(41);
+	triangles.push_back(33);
+	triangles.push_back(30);
+
+	triangles.push_back(40);
+	triangles.push_back(32);
+	triangles.push_back(28);
+
+	triangles.push_back(38);
+	triangles.push_back(30);
+	triangles.push_back(27);
+
+	triangles.push_back(41);
+	triangles.push_back(36);
+	triangles.push_back(28);
+
+	triangles.push_back(42);
+	triangles.push_back(39);
+	triangles.push_back(31);
+
+	triangles.push_back(27);
+	triangles.push_back(31);
+	triangles.push_back(39);
+
+	triangles.push_back(40);
+	triangles.push_back(37);
+	triangles.push_back(29);
 
 
 
